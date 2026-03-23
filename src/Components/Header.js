10fcaa2e85793ba9ff37622e4f32fabab56c7845
swaleha <swaleha2612000@gmail.com>
2026-03-23 16:31:20 -0400

    modify changes portfolio

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -47,11 +47,11 @@ class Header extends Component {
                 Resume
               </a>
             </li>
-            <li>
+            {/* <li>
               <a className="smoothscroll" href="#portfolio">
                 Works
               </a>
-            </li>
+            </li> */}
             
             <li>
               <a className="smoothscroll" href="#testimonials">
