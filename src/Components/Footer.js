d51a28b8401c61e5afa24b7ea7563854692f5677
swaleha <swaleha2612000@gmail.com>
2026-03-23 16:06:02 -0400

    fix build error

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -21,14 +21,17 @@ class Footer extends Component {
             <ul className="social-links">{networks}</ul>
 
                 <ul>
-                <li>Designed and developed by Swaleha<a title="Swaleha" href="#">
-                {/* <span role="img" aria-label="heart"></span>Swaleha <span role="img" aria-label="heart"></span> */}
-                  </a></li>
+                <li>Designed and developed by Swaleha
+                  <button
+                      type="button"
+                      title="Swaleha"
+                      className="link-button"
+                      onClick={() => {}}
+                    >
+                    
+                    </button>
+                  </li>
                 </ul>
-
-            {/* <ul className="copyright">
-              <li>&copy; 2024</li>
-            </ul> */}
           </div>
           <div id="go-top">
             <a className="smoothscroll" title="Back to Top" href="#home">
